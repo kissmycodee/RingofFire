581cbee520462c02febb244b14680d60c4d5f9c0
Orhan Kacar <orhankacar@web.de>
2023-07-24 01:05:47 +0200

    currentcard string problem

---
+++ src/app/game/game.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+import { Game } from 'src/models/game';
+import {MatDialog} from '@angular/material/dialog';
+import { DialogAddPlayerComponent } from '../dialog-add-player/dialog-add-player.component';
+
+
+@Component({
+  selector: 'app-game',
+  templateUrl: './game.component.html',
+  styleUrls: ['./game.component.scss'],
+})
+export class GameComponent implements OnInit {
+  pickCardAnimation = false;
+  game!: Game;
+  currentCard: string = '';
+
+  constructor(public dialog: MatDialog) {}
+
+  ngOnInit(): void {
+    this.newGame();
+  }
+
+  newGame() {
+    this.game = new Game();
+    console.log(this.game);
+  }
+
+  takeCard() {
+    if (!this.pickCardAnimation) {
+      this.currentCard = this.game.stack.pop();
+      this.pickCardAnimation = true;
+      console.log('Game is', this.game)
+      console.log('NEW CARD is:' + this.currentCard);
+
+      setTimeout(() => {
+      this.game.playedCards.push(this.currentCard)
+      this.pickCardAnimation = false;
+      }, 1000);
+    }
+  }
+
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(DialogAddPlayerComponent);
+
+    dialogRef.afterClosed().subscribe((name: string) => {
+      this.game.players.push(name)
+    });
+  }
+}
